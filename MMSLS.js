401fda5ab3ceea73018987d9d6313164f81501dd
fake-Q <84896722+fake-Q@users.noreply.github.com>
2023-05-19 09:03:58 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,6 +1,6 @@
 
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=27169508&pid=dc499dc74d0a3028edde1cba23882b78&tid=205a576a7d35169a1d670b9f9ead107b",
+    "https://www.maimemo.com/share/page?uid=27169508&pid=113fbd0409b03516a4f630ff44d7a2d6&tid=a265d48f43d78ed5382a631f040667bb",
   
 ]
   /**
