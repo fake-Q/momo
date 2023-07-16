bb9b6571171888a8b786a5130a9c7f3cb0453920
fake-Q <84896722+fake-Q@users.noreply.github.com>
2023-07-16 11:31:36 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,8 +1,12 @@
 
-const MMSLS =  [
-  "https://www.maimemo.com/share/page?uid=27169508&pid=d93c73d51c1ca60dae26589648d18bc4&tid=41be263dc9f79a4e0ab93d647c1b0020",
-]
-  * 生成随机数字
+const MMSLS = [
+    
+    "https://www.maimemo.com/share/page?uid=27169508&pid=3f0d7b869c48f46c08423e180d455f46&tid=1b84167617f89c03e5615bb2cf802f81",
+    "",
+    "",
+  ]
+  /**
+   * 生成随机数字
    * @param {number} min 最小值（包含）
    * @param {number} max 最大值（不包含）
    */
